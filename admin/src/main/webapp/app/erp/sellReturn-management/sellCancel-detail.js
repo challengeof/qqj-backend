ecf7f68745fddf30bfcaf0c37a9c61a3ae469e11
wangguodong577 <290639027@qq.com>
2016-04-11 17:22:07 +0800

    t

--- admin/src/main/webapp/app/erp/sellReturn-management/sellCancel-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('sbAdminApp')
-    .controller('SellCancelDetailCtrl', function ($scope, $http, $stateParams, $state) {
-
-        $http.get("/admin/api/sellCancel/" + $stateParams.id).success(function (data) {
-            $scope.sellCancel = data;
-        });
-    });
-
