ae4b63b41ec6287c2db789f672dcd0014325e1ed
wangguodong577 <290639027@qq.com>
2016-04-14 18:36:44 +0800

    t

--- admin/src/main/java/com/qqj/check/controller/CheckController.java
+++ admin/src/main/java/com/qqj/check/controller/CheckController.java
@@ -1,5 +1,7 @@
 package com.qqj.check.controller;
 
+import com.qqj.download.DownloadUtils;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,4 +25,10 @@ public class CheckController {
             })
     @ResponseBody
     public void adminAvilable() {}
+
+    @RequestMapping(value = "/api/download", method = RequestMethod.GET)
+    @ResponseBody
+    public HttpEntity<byte[]> exportPurchaseOrders() throws Exception {
+        return DownloadUtils.getHttpEntity();
+    }
 }
---
+++ core/src/main/java/com/qqj/download/DownloadUtils.java
@@ -0,0 +1,21 @@
+package com.qqj.download;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
+import java.io.File;
+
+/**
+ * Created by wangguodong on 16/4/14.
+ */
+public class DownloadUtils {
+    public static HttpEntity<byte[]> getHttpEntity() throws Exception {
+        byte[] readFileToByteArray = FileUtils.readFileToByteArray(new File("/tmp/com.mirror.cgwy_2.1.3_338.apk"));
+
+        HttpHeaders header = new HttpHeaders();
+        header.set("Content-Disposition", "attachment; filename=com.mirror.cgwy_2.1.3_338.apk");
+        header.setContentLength(readFileToByteArray.length);
+        return new HttpEntity<byte[]>(readFileToByteArray, header);
+    }
+}
