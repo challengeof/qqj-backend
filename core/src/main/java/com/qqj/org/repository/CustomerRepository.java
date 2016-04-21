ca9d5817d7ecf1791aee619911d99c82b23bab63
wangguodong577 <290639027@qq.com>
2016-04-21 23:41:09 +0800

    t

--- core/src/main/java/com/qqj/org/repository/CustomerRepository.java
+++ core/src/main/java/com/qqj/org/repository/CustomerRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,9 +14,9 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSp
 
     @Modifying
     @Query("update Customer c set c.rightCode = c.rightCode + 2 where c.rightCode >= :rightCode and c.team.id = :teamId")
-    void updateCustomerRightCode(Long rightCode, Long teamId);
+    void updateCustomerRightCode(@Param("rightCode") Long rightCode, @Param("teamId") Long teamId);
 
     @Modifying
     @Query("update Customer c set c.leftCode = c.leftCode + 2 where c.leftCode >= :rightCode and c.team.id = :teamId")
-    void updateCustomerLeftCode(Long rightCode, Long teamId);
+    void updateCustomerLeftCode(@Param("rightCode") Long rightCode, @Param("teamId") Long teamId);
 }
