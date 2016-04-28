033c94a1a9f6920bb5c8a8050cac264a8f723984
wangguodong577 <290639027@qq.com>
2016-04-28 12:24:10 +0800

    t

--- weixin/src/main/webapp/wxjs/test.js
+++ weixin/src/main/webapp/wxjs/test.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
+    var accessToken = '';
     $.ajax({
         url: "http://qiaoqianjin.boruifangzhou.com/wechat",
         type: "GET",
         success: function(data) {
+            alert(String data);
+            accessToken = data.accessToken;
+            alert(accessToken);
             wx.config({
                 debug: false,
-                appId: data.data.appId,
-                timestamp: data.data.timestamp,
-                nonceStr: data.data.noncestr,
-                signature: data.data.signature,
+                appId: data.appId,
+                timestamp: data.timestamp,
+                nonceStr: data.noncestr,
+                signature: data.signature,
                 jsApiList: [
                     'checkJsApi',
                     'onMenuShareTimeline',
@@ -45,37 +49,38 @@ $(document).ready(function(){
                     'openCard'
                 ]
             });
+            $('#uploadBtn').click(function(){
+                var localIds = '',
+                    serverId ='';
+                wx.chooseImage({
+                    count: 2, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                    success: function (res) {
+                        alert(1);
+                        localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    }
+                });
+                // alert(11);
+                wx.uploadImage({
+                    localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
+                    isShowProgressTips: 1, // 默认为1，显示进度提示
+                    success: function (res) {
+                        serverId = res.serverId; // 返回图片的服务器端ID
+                    }
+                });
+                $.ajax({
+                    url: "1.js",
+                    type: "POST",
+                    data: serverId,
+                    success: function(data) {
+                        $(this).css('background','red');
+                    }
+                })
+            })
         },
         error: function() {
 
         }
     });
-    $('#uploadBtn').click(function(){
-        var localIds = '',
-            serverId ='';
-        wx.chooseImage({
-            count: 2, // 默认9
-            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
-                localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-            }
-        });
-        // alert(11);
-        wx.uploadImage({
-            localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
-            isShowProgressTips: 1, // 默认为1，显示进度提示
-            success: function (res) {
-                serverId = res.serverId; // 返回图片的服务器端ID
-            }
-        });
-        $.ajax({
-            url: "1.js",
-            type: "POST",
-            data: serverId,
-            success: function(data) {
-                $(this).css('background','red');
-            }
-        })
-    })
 });
