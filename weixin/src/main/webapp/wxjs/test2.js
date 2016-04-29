eba2fa373d5c289ad4b46e6ca10289366c278b6f
wangguodong577 <290639027@qq.com>
2016-04-29 18:18:30 +0800

    h

---
+++ weixin/src/main/webapp/wxjs/test2.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+    var accessToken = '';
+    var appId = '';
+    $.ajax({
+        url: "http://www.boruifangzhou.com/wechat",
+        type: "GET",
+        success: function(data) {
+            accessToken = data.accessToken;
+            appId = data.appId;
+            //alert(accessToken);
+            wx.config({
+                debug: false,
+                appId: data.appId,
+                timestamp: data.timestamp,
+                nonceStr: data.noncestr,
+                signature: data.signature,
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'hideMenuItems',
+                    'showMenuItems',
+                    'hideAllNonBaseMenuItem',
+                    'showAllNonBaseMenuItem',
+                    'translateVoice',
+                    'startRecord',
+                    'stopRecord',
+                    'onRecordEnd',
+                    'playVoice',
+                    'pauseVoice',
+                    'stopVoice',
+                    'uploadVoice',
+                    'downloadVoice',
+                    'previewImage',
+                    'uploadImage',
+                    'downloadImage',
+                    'getNetworkType',
+                    'openLocation',
+                    'getLocation',
+                    'hideOptionMenu',
+                    'showOptionMenu',
+                    'closeWindow',
+                    'scanQRCode',
+                    'chooseWXPay',
+                    'openProductSpecificView',
+                    'addCard',
+                    'chooseCard',
+                    'openCard',
+		            'chooseImage',
+                    'previewImage',
+                    'uploadImage',
+                    'downloadImage'
+                ]
+            });
+
+            var codeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appId+"&redirect_uri=http://www.boruifangzhou.com/index.html&response_type=code&scope=snsapi_userinfo#wechat_redirect";
+            alert(codeUrl);
+            window.location.href="codeUrl";
+
+	wx.ready(function(){
+        //alert(222)
+        $('#uploadBtn').click(function(){
+                    var localIds = [],
+                        serverId =[];
+                    //alert(12211);
+            wx.chooseImage({
+                count: 2, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    //alert(localIds+"////");
+                    $.each( res.localIds, function(i, n){
+                        //alert(n);
+                        $(this).html('<img src="'+n.toString()+'" /> <br />')
+                        $(this).find('img').css({
+                             'height':'100%',
+                             'width': '100%'
+                         })
+                    });
+                },
+                fail: function(res){
+                //alert(112)
+                }
+            });
+
+            $('#btn').click(function(){
+                //alert(localIds);
+                wx.uploadImage({
+                    localId: localIds.toString(), // 需要上传的图片的本地ID，由chooseImage接口获得
+                    isShowProgressTips: 1, // 默认为1，显示进度提示
+                    success: function (res) {
+                        serverId = res.serverId; // 返回图片的服务器端ID
+                        alert('serverId:'+serverId);
+                        alert('accessToken:'+accessToken);
+                    },
+                    fail: function(res){
+                        alert(JSON.stringify(res))
+                    }
+                });
+            });
+
+
+            $('#saveBtn').click(function(){
+                var user = {};
+                user.serverId = serverId;
+                user.openId =  new Date().getTime();
+                user.name = 'tName';
+                user.telephone = '13756648000';
+                user.birthday = new Date(1986,3,7);
+                user.accessToken = accessToken;
+                alert(JSON.stringify(user))
+                $.ajax({
+                    url: "http://www.boruifangzhou.com/api/weixin/user/add",
+                    type: "post",
+                    data: JSON.stringify(user),
+                    contentType: "application/json",
+                    dataType: "json",
+                    success: function(data) {
+                        alert(JSON.stringify(data))
+                        $(this).css('background','green');
+                    },
+                    error: function(res) {
+                        alert(JSON.stringify(res));
+                    }
+                 })
+            })
+        })
+	});
+	wx.error(function(res){
+		alert(JSON.stringify(res));
+	})
+        },
+        error: function() {
+
+        }
+    });
+});
